903e559eca62a07f18018df40dbf81afd8d71811
Aditya Goswami <adistarted@gmail.com>
2025-07-18 04:21:59 +0000

    mathematics

---
+++ basic /calculation.py
@@ -0,0 +1,11 @@
+#addition two numbers by input 
+a=int(input("Enter the number a: "))
+b=int(input("Enter the number b: "))
+print("This is square of a ",a**2)#for square use a**2,not use a^2
+print("sum of two numbers is ",a+b)
+#average 
+print("Average of two numbers is ",(a+b)/2)
+#Reminder
+c=int(input("enter the number c: "))
+d=int(input("enter the number d: "))
+print("reminder of two numbers is ",c%d)
