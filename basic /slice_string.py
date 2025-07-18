fd1f53eda3478d64667d2a8c97d93b06d7fedc8b
Aditya Goswami <adistarted@gmail.com>
2025-07-18 08:40:14 +0000

    STRING SLICE

---
+++ basic /slice_string.py
@@ -0,0 +1,8 @@
+name="ADITYA"
+nameshort=name[0:3]
+print(nameshort)
+print(name[-4:-1])#same as name[1:4]
+print(name[:4])#same as name[0:4]
+print(name[0:])#same as name[0:5]
+#SKIPING VALUE
+print("SKIP NAME:",name[0:6:2])
