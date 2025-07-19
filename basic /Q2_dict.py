922406238d8e2e5d58c6a0fd328e640ac2ee9675
Aditya Goswami <adistarted@gmail.com>
2025-07-19 15:55:17 +0000

    In this 4frnd with language in dictionary

---
+++ basic /Q2_dict.py
@@ -0,0 +1,17 @@
+d={}
+name=input("Enter your name: ")
+lang=input("Enter your language: ")
+d.update({name:lang})
+name=input("Enter your name: ")
+lang=input("Enter your language: ")
+d.update({name:lang})
+name=input("Enter your name: ")
+lang=input("Enter your language: ")
+d.update({name:lang})
+name=input("Enter your name: ")
+lang=input("Enter your language: ")
+d.update({name:lang})
+print(d)
+#if two name are same than key updated or if values same nothing change values can write double 
+#let rohan: python next rohan:cpp
+#output--- rohan:cpp
